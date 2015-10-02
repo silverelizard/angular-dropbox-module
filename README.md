7b9f52d42c10308a3cc8daa6a60c81d3067b68ce
Corrine Olson <corrine.olson@careerbuilder.com>
2015-10-02 00:36:29 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # angular-dropbox-module
-A simple Angular module and directive for implementing the Dropbox Chooser API.
+A simple Angular module and directive for implementing the [Dropbox Chooser API](https://www.dropbox.com/developers/chooser). This directive uses `bindToController` in the directive definition, which requires the use of Angular 1.3+.
+
+In order to use the code, include the script in your page using your desired method. Once done, you can access the following settings on the `DropBoxSettingsProvider`:
+
+- `linkType`: accepts 'preview' or 'direct' (default is 'preview')
+- `multiselect`: accepts *true* or *false* (default is *false*)
+- `extensions`: accepts array of string file extension (default is `['.pdf', '.doc', '.docx']`)
+
+Example Usage:
+
+```JavaScript
+angular
+    .module('app', [dropbox-picker'])
+    .config(['DropBoxSettingsProvider', function(DropBoxSettingsProvider) {
+      DropBoxSettingsProvider.configure({
+        linkType: 'direct',
+        multiselect: false,
+        extensions: ['.pdf', '.doc', '.docx', '.txt', '.odt', '.rtf']
+      });
+    }]);
+```
---
+++ dropbox-picker-directive.js
@@ -0,0 +1,67 @@
+(function DBDirectiveModule() {
+  'use strict';
+
+  angular.module("dropbox-picker", [])
+    .provider("DropBoxSettings", function() {
+      this.linkType = 'preview';
+      this.multiselect = false;
+      this.extensions = ['.pdf', '.doc', '.docx'];
+      this.$get = function() {
+        return {
+          linkType: this.linkType,
+          multiselect: this.multiselect,
+          extensions: this.extensions
+        }
+      };
+      this.configure = function(e) {
+        for (var key in e) this[key] = e[key]
+      };
+    })
+    .controller('dbController', DBController)
+    .directive("dropBoxPicker", ["DropBoxSettings",
+      function(DropBoxSettings) {
+        return {
+          restrict: "A",
+          scope: {
+            dbpickerFiles: "="
+          },
+          bindToController: true,
+          controller: 'dbController',
+          controllerAs: 'vm',
+          link: function(scope, element, attrs) {
+            element.bind("click", function() {
+              scope.vm.instantiate()
+            })
+          }
+        }
+      }
+    ]);
+
+  DBController.$inject = ['$scope', 'DropBoxSettings'];
+
+  function DBController($scope, dropBoxSettings) {
+    var vm = this;
+    vm.instantiate = instantiate;
+    vm.dropboxSuccess = dropboxSuccess;
+
+    vm.dropboxOptions = {
+      success: vm.dropboxSuccess,
+      cancel: function() {},
+      linkType : dropBoxSettings.linkType,
+      multiselect: dropBoxSettings.multiselect,
+      extensions : dropBoxSettings.extensions
+    };
+
+    function dropboxSuccess(files){
+      $scope.$apply(function() {
+        for (var i = 0; i < files.length; i++) {
+          vm.dbpickerFiles.push(files[i]);
+        }
+      });
+    }
+
+    function instantiate() {
+      Dropbox.choose(vm.dropboxOptions);
+    }
+  }
+})();
